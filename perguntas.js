86552618cf115eba901e0a1345189fc2cd1fc40e
lorraine0212 <goncalves.lorraine@escola.pr.gov.br>
2025-08-14 16:47:34 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'Qual é o rio mais longo do mundo?',
+    ' Rio Nilo'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital do Brasil?',
+    ' Brasília'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual deserto é considerado o maior do mundo?',
+    ' Deserto da Antártida'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a montanha mais alta do mundo?' ,
+    ' Monte Everest'
+)
